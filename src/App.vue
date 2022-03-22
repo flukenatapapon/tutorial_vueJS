5c2209db134a90648aa14c82065394154c81f649
natapapon <natapapon.r@gmail.com>
2022-03-22 16:56:58 +0700

    vue tutorials todo app

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,112 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <img :src="logoURL" :alt="logoCaption" width="200" height="200">
+    <h1>{{ title }}</h1>
+
+    <div>
+      <span>You have {{ allTasks }} {{ allTasks > 1 ? 'tasks' : 'task' }} at the moment</span>
+      <br>
+      <input type="text"
+        v-model="newTask"
+        placeholder="Add a new task"
+        @keyup.enter="addTask()"
+      >
+
+      <button @click="addTask()" :disabled="newTask.length < 1">
+        Add Task
+      </button>
+    </div>
+    <div v-if="newTask.length > 0">
+      <h3>New Task preview</h3>
+      <p>{{ newTask }}</p>
+    </div>
+    <ul>
+      <li 
+        v-for="(task, index) in lastest"
+       :key="task.id"
+       @click="finishTask(task)"
+       :class="{ strikeout: task.finished }"
+      >
+        {{ index + 1 }}. {{ task.name }}
+
+        <div v-if="task.finished">
+          <button @click="removeTask(task.id)">Delete task</button>
+        </div>
+        <div v-else-if="task.edit">
+          <button>Edit task</button>
+        </div>
+        <div v-else>
+          <p>No Button</p>
+        </div>
+      </li>
+    </ul>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      title: 'My To-Do App',
+      newTask: '',
+      logoURL: 'https://images.unsplash.com/photo-1586281380117-5a60ae2050cc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+      logoCaption: 'Learn how to build todo app with vue 3',
+      tasks: [
+        {
+          id: 1,
+          name: 'Learn VUE js', 
+          finished: true
+        },
+        {
+          id: 2,
+          name: 'Build a Vue App', 
+          finished: false
+        },
+        {
+          id: 3,
+          name: 'Build Todo App', 
+          finished: false
+        }        
+      ]
+    }
+  },
+  methods: {
+    addTask() {
+      if (this.newTask.length < 1) return;
+
+      this.tasks.push({
+        id: this.tasks.length + 1,
+        name: this.newTask,
+        finished: false
+      });
+
+      this.newTask = ''
+    },
+    finishTask(task) {
+      task.finished = !task.finished;
+    },
+    removeTask(taskID) {
+      this.tasks = this.tasks.filter(task => {
+        return task.id !== taskID
+      })
+    }
+  },
+  computed: {
+    allTasks() {
+      return this.tasks.length;
+    },
+    lastest() {
+      return [...this.tasks].reverse();
+    }
   }
 }
 </script>
 
 <style>
+
+.strikeout {
+  text-decoration: line-through;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
